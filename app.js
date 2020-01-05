94eaae14621ceb342800af6f6edcb2df85a8e6e1
fonsecaBarreto <lucasbfonte@gmail.com>
2020-01-05 00:17:46 -0300

    nice

--- app.js
+++ app.js
@@ -20,8 +20,101 @@ server.listen(3000,()=>{
 
 
 });
-
-io.on("connection",function(client){
+var colors =[[191,191,63],[191,63,63],[232,32,122],[200,32,32]] 
+var players = {}
+var elements = [new Body([-300,-300,600,60],200),new Body([-300,300,600,60],200),new Body([-360,-300,60,660],200)]
+var lastPosition = [0,0]
+io.on("connection",function(socket){
   console.log("novo cliente")
-  client.emit("welcome",{id:client.id})
-})
+  players[socket.id]= new Player([lastPosition[0]+64,lastPosition[1],64,96],colors[Math.floor(Math.random()*colors.length)]);
+  players[socket.id].id = socket.id
+  lastPosition[0]=players[socket.id].position[0]
+  socket.emit("welcome",JSON.stringify({players,elements:elements}))
+  socket.broadcast.emit("welcome",JSON.stringify({players,elements:elements}))
+  /*  */
+  socket.on("input",(msg)=>{
+    let client = JSON.parse(msg);
+ 
+    let player = players[client.playerId]
+      client.keys[65] == 1 ? player.position[0]-=player.speed: null;
+      client.keys[68] == 1 ? player.position[0]+=player.speed: null;
+
+    client.keys[83] == 1 ? player.position[1]+=player.speed: null;
+    client.keys[87] == 1 ? player.position[1]-=player.speed: null;
+    //detectColision(player,Object.keys(players).map(key=>players[key]))
+    detectColision(player,elements)
+    updateClients(socket,client.playerId);
+   
+    
+  });
+  socket.on("disconnect",function(scoket){
+    console.log(socket.id, "has disconnected");
+    delete players[socket.id];
+    updateClients(socket,socket.id);
+    
+  })
+
+  
+});
+function detectColision(player,elements){
+  elements.forEach(corpo=>{
+     var d1x = corpo.position[0]  - (player.position[0]+player.width);
+     var d1y = corpo.position[1]  - (player.position[1]+player.height);
+     var d2x = player.position[0] - (corpo.position[0] +corpo.width);
+     var d2y =player.position[1] -  (corpo.position[1] +corpo.height);
+
+     if(d1x > 0 || d1y > 0){}
+     else if(d2x > 0 || d2y > 0){}
+     else{
+       if(d1x > d2x && d1x > d1y && d1x > d2y ){
+        player.position[0]=corpo.position[0]-player.width;
+        
+      } 
+       else if(d2x > d1x  && d2x > d1y && d2x > d2y){
+         player.position[0] = corpo.position[0]+corpo.width;
+        
+       
+       
+        } 
+       else if(d1y > d2x && d1y > d1x && d1y > d2y ){
+         player.position[1] = corpo.position[1]-player.height;
+       
+         
+      
+        } 
+       else if(d2y > d1x  && d2y > d1y && d2y > d2x){
+         player.position[1] = corpo.position[1]+corpo.height;
+        
+ 
+       }
+     }
+  })
+}
+function updateClients(socket,id){
+  socket.emit("update",JSON.stringify(id in players ? players : id));
+  socket.broadcast.emit("update",JSON.stringify(id in players ? players : id))
+}
+function Piso(){
+  
+}
+function Body(rect = [0,0,32,32],color = 123){
+  this.color = color;
+  this.position = [rect[0],rect[1]];
+  this.width = rect[2];
+  this.height = rect[3];
+  this.speed = 10;
+  this.velocity = [0,0]
+  this.stroke = 100;
+  this.animation = [];
+  this.update = ()=>{
+    this.position[0]+=this.velocity[0];
+    this.position[1]+=this.velocity[1];
+  };
+ 
+}
+function Player(rect,color){
+  Body.call(this,rect,color);
+  
+}
+
+
--- sketch.js
+++ sketch.js
@@ -2,14 +2,33 @@
 var keys = {}
 /*  */
 var WIDTH= 1080, HEIGHT = 720;
-var ROOT = [WIDTH/2,HEIGHT/2]
-var player =  new Player([0,0,64,96],180);
-var player2 =  new Player([100,0,64,96],180);
-var elements = [player, player2, new Body([-300,300,100,100]),new Body([-200,100,100,100]),new Body([300,-150,100,100]),new Body([200,180,100,100])];
+var ROOT = [WIDTH/2,HEIGHT/2];
+players = {};
+var playerId = null;
+var elements = [];
 var cam = {
   position : [0,0],
   speed : 10
 }
+window.onload = ()=>{
+  socket = io.connect('http://10.0.10.193:3000'); 
+  socket.on("connect",function(){
+    socket.on("welcome",function(msg){
+      resp = JSON.parse(msg);
+      players = resp.players;
+      elements = resp.elements;
+      playerId = socket.id
+      
+    });
+    socket.on("update",function(msg){
+      let updated = JSON.parse(msg);
+      typeof(updated) == "object" ? players = updated : delete players[updated];
+    
+
+    })
+  })
+}
+
 function preload() {
   spritedata = loadJSON("/char.json");
   spritesheet = loadImage("/char.png")
@@ -31,88 +50,45 @@ function drawGrid(){
   line(ROOT[0]-cam.position[0],ROOT[1]-cam.position[1],ROOT[0]-cam.position[0],0-cam.position[1])
   line(ROOT[0]-cam.position[0],ROOT[1]-cam.position[1],ROOT[0]-cam.position[0],HEIGHT-cam.position[1])
 }
+function inputHandle(){
+  if(Object.keys(keys).length > 0){
+    socket.emit("input",JSON.stringify({keys,playerId}))
+  }
+ 
+  //cam.position[0]+=  (keys[68] ? cam.speed: 0) - (keys[65] ? cam.speed : 0 ) ;
+  //cam.position[1]+=  (keys[83] ? cam.speed: 0) - (keys[87] ? cam.speed : 0 ) ; 
+  cam.position[0]=  players[playerId].position[0]+players[playerId].width/2
+  cam.position[1]=  players[playerId].position[1]+players[playerId].height/2
+  //cam.position[1]=  (keys[83] ? cam.speed: 0) - (keys[87] ? cam.speed : 0 ) ; 
+ 
+}
 function draw() {
+ 
+
+    inputHandle();
     background(220);
-    cam.position[0]+=  (keys[68] ? cam.speed: 0) - (keys[65] ? cam.speed : 0 ) ;
-    cam.position[1]+=  (keys[83] ? cam.speed: 0) - (keys[87] ? cam.speed : 0 ) ;
-    player.velocity[0] =  (keys[68] == undefined ? 0: keys[68]  * player.speed) - (keys[65] == undefined ? 0: keys[65]  * player.speed) ;
-    player.velocity[1] =  (keys[83] == undefined ? 0: keys[83]  * player.speed) - (keys[87] == undefined ? 0: keys[87]  * player.speed) ;
-    
-    player.update();
-    detectColision(player)
-    render(player,cam);
-    elements.forEach(element => {render(element,cam)});
-  /* dev */
+    if(Object.keys(players).length > 0){
+      Object.keys(players).forEach(key=>{
+        let player = players[key];
+        render(player,cam);
+        document.querySelector("#player-position").innerHTML = `${players[playerId].position[0]}, ${players[playerId].position[1]}`;
+      })
+    }
+    if(elements.length > 0){
+      elements.forEach(element => {render(element,cam)});
+    }
     drawGrid(cam);
-    document.querySelector("#player-position").innerHTML = `${player.position[0]}, ${player.position[1]}`
+    
 }
 function render(element,cam){
   fill(element.color);
   stroke(element.stroke);
   rect(ROOT[0]-cam.position[0]+element.position[0],ROOT[1]-cam.position[1]+element.position[1],element.width,element.height);
+}
 
-  /* if(element.animation == undefined){
-    rect(ROOT[0]-cam.position[0]+element.position[0],ROOT[1]-cam.position[1]+element.position[1],element.width,element.height);
-  }else{
-    image(element.animation[frameCount % element.animation.length], ROOT[0]-cam.position[0]+element.position[0],ROOT[1]-cam.position[1]+element.position[1],element.width,element.height);
-  } */
-}
-function detectColision(player){
-  elements.forEach(corpo=>{
-     var d1x = corpo.position[0]  - (player.position[0]+player.width);
-     var d1y = corpo.position[1]  - (player.position[1]+player.height);
-     var d2x = player.position[0] - (corpo.position[0] +corpo.width);
-     var d2y =player.position[1] -  (corpo.position[1] +corpo.height);
-
-     if(d1x > 0 || d1y > 0){}
-     else if(d2x > 0 || d2y > 0){}
-     else{
-      
-       if(d1x > d2x && d1x > d1y && d1x > d2y ){
-        player.position[0]=corpo.position[0]-player.width;
-        cam.position[0]=corpo.position[0]-player.width;
-    
-      } 
-       else if(d2x > d1x  && d2x > d1y && d2x > d2y){
-         player.position[0] = corpo.position[0]+corpo.width
-         cam.position[0] = corpo.position[0]+corpo.width
-       
-        } 
-       else if(d1y > d2x && d1y > d1x && d1y > d2y ){
-         player.position[1] = corpo.position[1]-player.height
-         cam.position[1] = corpo.position[1]-player.height
-      
-        } 
-       else if(d2y > d1x  && d2y > d1y && d2y > d2x){
-         player.position[1] = corpo.position[1]+corpo.height
-         cam.position[1] = corpo.position[1]+corpo.height
- 
-       }
-     }
-  })
-}
-function keyPressed(){
-  keys[keyCode]=1;
-}
+function keyPressed(){keys[keyCode]=1;}
 function keyReleased(){
-  keys[keyCode]=0;
-}
-function Body(rect = [0,0,32,32],color = 123){
-  this.color = color;
-  this.position = [rect[0],rect[1]];
-  this.width = rect[2];
-  this.height = rect[3];
-  this.speed = 10;
-  this.velocity = [0,0]
-  this.color = 200;
-  this.stroke = 100;
-  this.update = ()=>{
-    this.position[0]+=this.velocity[0];
-    this.position[1]+=this.velocity[1];
-  };
- 
-}
-function Player(rect,color){
-  Body.call(this,rect,color);
-  this.animation = [];
+  keys[keyCode]=undefined;
+  delete keys[keyCode];
+  socket.emit("input",JSON.stringify({keys,playerId}))
 }
